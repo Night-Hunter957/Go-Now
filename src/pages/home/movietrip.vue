f36df6dccf80ea716b448b6d7b25541bdffdcf10
JRs007 <451645927@qq.com>
2018-01-09 10:06:03 +0800

    首页初步完成设计

--- src/pages/home/movietrip.vue
+++ src/pages/home/movietrip.vue
@@ -29,16 +29,16 @@
   import BScroll from 'better-scroll'
   export default{
     name: 'movietrip',
-    props: { 
+    props: {
       movietripInfo: Array
     },
-    mounted() {
-     this.scroll = new BScroll(this.$refs.wrapper, {
-     	scrollX: true,
-     	scrollY: false,
-     	eventPassthrough: 'vertical',
-     	click: true
-     })
+    mounted () {
+      this.scroll = new BScroll(this.$refs.wrapper, {
+        scrollX: true,
+        scrollY: false,
+        eventPassthrough: 'vertical',
+        click: true
+      })
     }
 }
 </script>
