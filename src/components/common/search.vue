86f212b9bfc006b74e3be6713b97e17a780cf1c7
JRs007 <451645927@qq.com>
2018-01-12 16:55:22 +0800

    注册页加了正则判断

--- src/components/common/search.vue
+++ src/components/common/search.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="search">
   	<div class="logo">
-     <img class="logo-img" src="../../assets/logo.jpg"> 
+     <img class="logo-img" src="@/../static/img/logo.png"> 
     </div>
-  	<div class="search-input"><input  class="cont" type="text" placeholder="旅行年货大街正式开卖"></div>
+  	<div class="search-input"><input  class="cont" type="text" placeholder="搜索目的地/景点/酒店等"></div>
   	<div class="mes">
      <img class="mes-img" src="../../assets/message.png">  
     </div>
@@ -34,17 +34,20 @@
       flex:1
       height: .63rem
       line-height: 1.3rem
-      background: yellow
+      /*background: yellow*/
       margin:0.34rem .47rem 0 .25rem
       .cont
         position: absolute
         text-align: center
         width:100%
         height:100%
+        height:.6rem
+        border:1px solid #808080
+        border-radius:3px
     .mes 
       float:left
-      width:.36rem
-      height:.4rem
+      width:.44rem
+      height:.51rem
       line-height: 1.3rem
       margin-right:.47rem
       margin-top:.45rem
